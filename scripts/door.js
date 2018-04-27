2208e82dbceba2e0074e07781faefedf352d53c2
Jake Mauer <jake@jakemauer.com>
2018-04-27 14:37:45 -0700

    Trying to change ordering

--- scripts/door.js
+++ scripts/door.js
@@ -7,9 +7,9 @@ var kisiPassword = process.env.KISI_PASSWORD;
 module.exports = function(robot) {
   return robot.hear(/let me in/i, function(res) {
     return kisiClient.signIn(kisiUsername, kisiPassword).
+      then(function() { res.reply("Opening the doors") }).
       then(function() { kisiClient.post("locks/3697/unlock") }).
       then(function() { kisiClient.post("locks/3698/unlock") }).
-      then(function() { res.reply("Unlocking both :door:s") }).
       catch(function() { res.reply("I can't open the :door:") })
   })
 }
