bd6b7ef237b88d3f7f0f6660fe46b7bf93abc79e
Jake Mauer <jake@jakemauer.com>
2018-04-27 14:21:01 -0700

    Replace mark’s api key with ones provided as env vars

--- scripts/door.js
+++ scripts/door.js
@@ -1,9 +1,12 @@
 Kisi = require('kisi-client');
 kisiClient = new Kisi.default();
 
+const kisiUsername = process.env['KISI_USERNAME'];
+const kisiApiKey = process.env['KISI_API_KEY'];
+
 module.exports = function(robot) {
   return robot.hear(/let me in/i, function(res) {
-    return kisiClient.signIn('mark@allovue.com', 'Fs5-Hdb-FGN-NNS').
+    return kisiClient.signIn(kisiUsername, kisiApiKey).
       then(function() { kisiClient.post("locks/3697/unlock") }).
       then(function() { kisiClient.post("locks/3698/unlock") }).
       catch(function() { res.reply("I can't open the :door:") })
