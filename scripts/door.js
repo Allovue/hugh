3c1becee8390aff239360fbc1dda4d87915340de
Jake Mauer <jake@jakemauer.com>
2018-04-27 14:30:00 -0700

    Ahh so you use a password and not an API key

--- scripts/door.js
+++ scripts/door.js
@@ -2,11 +2,11 @@ Kisi = require('kisi-client');
 kisiClient = new Kisi.default();
 
 var kisiUsername = process.env.KISI_USERNAME;
-var kisiApiKey = process.env.KISI_API_KEY;
+var kisiPassword = process.env.KISI_PASSWORD;
 
 module.exports = function(robot) {
   return robot.hear(/let me in/i, function(res) {
-    return kisiClient.signIn(kisiUsername, kisiApiKey).
+    return kisiClient.signIn(kisiUsername, kisiPassword).
       then(function() { kisiClient.post("locks/3697/unlock") }).
       then(function() { kisiClient.post("locks/3698/unlock") }).
       catch(function() { res.reply("I can't open the :door:") })
