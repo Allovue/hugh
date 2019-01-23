1e60d7a353a63dd33ce79b497bf637b57c94503f
Jake Mauer <jake@jakemauer.com>
2019-01-22 23:01:27 -0800

    Polish script

--- scripts/prodpad-feedback.js
+++ scripts/prodpad-feedback.js
@@ -1,18 +1,19 @@
-module.exports = function(robot) {
+// This is using Jason's key on production as of 1/22/19
+var prodpadKey = process.env.PRODPAD_API_KEY;
 
+module.exports = function(robot) {
   function sendFeedback(feedbackData, slackRes) {
     var data = JSON.stringify(feedbackData);
 
     robot.http("https://api.prodpad.com/v1/feedbacks")
       .header('Content-Type', 'application/json')
-      .header('Authorization', `Bearer ${process.env.PRODPAD_API_KEY}`)
+      .header('Authorization', `Bearer ${prodpadKey}`)
       .post(data)(function(err, res, body) {
         if (err) {
-          return slackRes.reply(`Well dang there was an error sending your feedback:\n ${err}`)
+          return slackRes.reply(`Well dang, there was an error sending your feedback:\n ${err}`)
         }
-        return slackRes.reply("Your feedback was received!")
+        return slackRes.reply("Your feedback was successfully added to ProdPad :)")
       })
-
   };
 
   robot.hear(/feedback (.*)/i, function(res) {
@@ -24,8 +25,3 @@ module.exports = function(robot) {
     return sendFeedback(feedbackData, res);
   })
 }
-
-
-
-
-
