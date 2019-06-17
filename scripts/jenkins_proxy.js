d0f6a3ae7a24b4a7dab42a61bf8ed5e90a56fa81
Paul Ostazeski <paul.ostazeski@gmail.com>
2019-06-17 14:15:02 -0400

    Better name for copy to staging

--- scripts/jenkins_proxy.js
+++ scripts/jenkins_proxy.js
@@ -2,9 +2,10 @@ var jenkinsURL = process.env.JENKINS_URL;
 var jenkinsToken = process.env.JENKINS_TOKEN;
 
 module.exports = function(robot) {
-  robot.respond(/setup (.+)-staging/i, function(res) {
+  robot.respond(/copy (.+) to staging/i, function(res) {
     var jobName = 'DB_copy'
     var customer = res.match[1];
+    if (customer == "demo" || customer == "development") { customer = "demo" };
     robot.http(jenkinsURL + '/buildByToken/buildWithParameters?job=' + jobName + '&token=' + jenkinsToken + '&customer=' + customer).post(null) (function(err, res, body) {
       return true;
     });
