18c4a927304f6169a618e64010b04169de35a357
Paul Ostazeski <paul.ostazeski@gmail.com>
2019-08-05 12:04:04 -0400

    Avoid ambiguous "res" variable name

--- scripts/jenkins_proxy.js
+++ scripts/jenkins_proxy.js
@@ -2,38 +2,38 @@ var jenkinsURL = process.env.JENKINS_URL;
 var jenkinsToken = process.env.JENKINS_TOKEN;
 
 module.exports = function(robot) {
-  robot.respond(/copy (.+) to staging/i, function(res) {
+  robot.respond(/copy (.+) to staging/i, function(msg) {
     var jobName = 'DB_copy'
-    var customer = res.match[1];
+    var customer = msg.match[1];
     if (customer == "demo" || customer == "development") { customer = "demo" };
-    robot.http(jenkinsURL + '/buildByToken/buildWithParameters?job=' + jobName + '&token=' + jenkinsToken + '&customer=' + customer).post(null) (function(err, res, body) {
+    robot.http(jenkinsURL + '/buildByToken/buildWithParameters?job=' + jobName + '&token=' + jenkinsToken + '&customer=' + customer).post(null) (function(err, response, body) {
       if (err) {
-        return res.reply("I can't do that right now");
+        return msg.reply("I can't do that right now");
       } else {
-        return res.reply("Copying " + customer + " to staging");
+        return msg.reply("Copying " + customer + " to staging");
       }
     });
   })
 
-  robot.respond(/I need a database dump for (.+)/i, function(res) {
+  robot.respond(/I need a database dump for (.+)/i, function(msg) {
     var jobName = "Get%20DB%20dump%20for%20developer"
-    var customer = res.match[1];
-    robot.http(jenkinsURL + '/buildByToken/buildWithParameters?job=' + jobName + '&token=' + jenkinsToken + '&customer=' + customer).post(null) (function(err, res, body) {
+    var customer = msg.match[1];
+    robot.http(jenkinsURL + '/buildByToken/buildWithParameters?job=' + jobName + '&token=' + jenkinsToken + '&customer=' + customer).post(null) (function(err, response, body) {
       if (err) {
-        return res.reply("I can't do that right now");
+        return msg.reply("I can't do that right now");
       } else {
-        return res.reply("Backing up " + customer);
+        return msg.reply("Backing up " + customer);
       }
     });
   })
 
-  robot.respond(/restart the elasticsearch cluster/i, function(res) {
+  robot.respond(/restart the elasticsearch cluster/i, function(msg) {
     var jobName = "Restart%20elasticsearch%20cluster"
-    robot.http(jenkinsURL + '/buildByToken/build?job=' + jobName + '&token=' + jenkinsToken).post(null) (function(err, res, body) {
+    robot.http(jenkinsURL + '/buildByToken/build?job=' + jobName + '&token=' + jenkinsToken).post(null) (function(err, response, body) {
       if (err) {
-        return res.reply("I can't do that right now");
+        return msg.reply("I can't do that right now");
       } else {
-        return res.reply("Attempting cluster restart.");
+        return msg.reply("Attempting cluster restart.");
       }
     });
   });
