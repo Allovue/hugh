be2697c76b2855d6b09e08060be763b856ae4106
Paul Ostazeski <paul.ostazeski@gmail.com>
2019-09-05 18:20:53 -0400

    Add handler for kicking off ETL processes
    Why:
    * I want to minimize the amount of people fiddling about in jenkins.
    
    This change addresses the need by:
    * Adding another hugh response.
    * Extracting some repeated code into a url-building method.

--- scripts/jenkins_proxy.js
+++ scripts/jenkins_proxy.js
@@ -2,16 +2,28 @@
 //   hubot copy <customer> to staging - triggers an background process to clone production data to staging (note that demo copiee to development)
 //   hubot I need a database dump for <customer|demo> - triggers a background process to generate a postgres dump file to be generated suitable for `pg_restore` locally.
 //   hubot restart the elasticsearch cluster - turns elasticsearch nodes off and back on again. Do this if (and only if) we're seeing a slew of errors and etl failures in a span of seconds/minutes.
+//   hubot start the ETL for <customer> - starts the Extract/Transform/Load process to import data to Balance for the named customer. For a TINY number of customers that process does not include "extract".
 
 var jenkinsURL = process.env.JENKINS_URL;
 var jenkinsToken = process.env.JENKINS_TOKEN;
 
+function urlFor(jobName, customer) {
+  var url;
+  if (customer) {
+    url = jenkinsURL + '/buildByToken/buildWithParameters?job=' + jobName + '&token=' + jenkinsToken + '&customer=' + customer;
+  } else {
+    url = jenkinsURL + '/buildByToken/build?job=' + jobName + '&token=' + jenkinsToken;
+  }
+  return url;
+};
+
 module.exports = function(robot) {
-  robot.respond(/copy (.+) to staging/i, function(msg) {
-    var jobName = 'DB_copy'
+  robot.respond(/copy (\w+) to staging/i, function(msg) {
+    var jobName = 'DB_copy';
     var customer = msg.match[1];
     if (customer == "demo" || customer == "development") { customer = "demo" };
-    robot.http(jenkinsURL + '/buildByToken/buildWithParameters?job=' + jobName + '&token=' + jenkinsToken + '&customer=' + customer).post(null) (function(err, response, body) {
+
+    robot.http(urlFor(jobName, customer)).post(null) (function(err, response, body) {
       if (err) {
         return msg.reply("I can't do that right now");
       } else {
@@ -20,10 +32,11 @@ module.exports = function(robot) {
     });
   })
 
-  robot.respond(/I need a database dump for (.+)/i, function(msg) {
-    var jobName = "Get%20DB%20dump%20for%20developer"
+  robot.respond(/I need a database dump for (\w+)/i, function(msg) {
+    var jobName = "Get%20DB%20dump%20for%20developer";
     var customer = msg.match[1];
-    robot.http(jenkinsURL + '/buildByToken/buildWithParameters?job=' + jobName + '&token=' + jenkinsToken + '&customer=' + customer).post(null) (function(err, response, body) {
+
+    robot.http(urlFor(jobName, customer)).post(null) (function(err, response, body) {
       if (err) {
         return msg.reply("I can't do that right now");
       } else {
@@ -32,9 +45,23 @@ module.exports = function(robot) {
     });
   })
 
+  robot.respond(/start the ETL for (\w+)/, function(msg) {
+    var customer = msg.match[1];
+    var jobName = "ETL%2fhubot%20handler";
+
+    robot.http(urlFor(jobName, customer)).post(null) (function(err, response, body) {
+      if (err) {
+        return msg.reply("I can't do that right now");
+      } else {
+        return msg.reply("Starting ETL for " + customer);
+      }
+    });
+  });
+
   robot.respond(/restart the elasticsearch cluster/i, function(msg) {
-    var jobName = "Restart%20elasticsearch%20cluster"
-    robot.http(jenkinsURL + '/buildByToken/build?job=' + jobName + '&token=' + jenkinsToken).post(null) (function(err, response, body) {
+    var jobName = "Restart%20elasticsearch%20cluster";
+
+    robot.http(urlFor(jobName)).post(null) (function(err, response, body) {
       if (err) {
         return msg.reply("I can't do that right now");
       } else {
