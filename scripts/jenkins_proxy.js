fa1a72e23bba100e92389b469ab6b9b705869e03
Paul Ostazeski <paul.ostazeski@gmail.com>
2019-09-24 13:48:58 -0400

    Add second parameter to "hubot start the ETL for customer"
    Why:
    * To allow importing non-default years' data for a customer

--- scripts/jenkins_proxy.js
+++ scripts/jenkins_proxy.js
@@ -3,12 +3,13 @@
 //   hubot I need a database dump for <customer|demo> - triggers a background process to generate a postgres dump file to be generated suitable for `pg_restore` locally.
 //   hubot restart the elasticsearch cluster - turns elasticsearch nodes off and back on again. Do this if (and only if) we're seeing a slew of errors and etl failures in a span of seconds/minutes.
 //   hubot start the ETL for <customer> - starts the Extract/Transform/Load process to import data to Balance for the named customer. For a TINY number of customers that process does not include "extract".
+//   hubot start the ETL for <customer> using <non-default-import_bucket_1,non-default-import_bucket_2> - starts the Extract/Transform/Load process to import data to Balance for the named customer. Overrides the per-customer vault value for "import_buckets" to allow prior years data to be loaded.
 //   hubot reindex elasticsearch for <customer> - performs just the elasticsearch-updating portion of the ETL process for the named customer.
 
 var jenkinsURL = process.env.JENKINS_URL;
 var jenkinsToken = process.env.JENKINS_TOKEN;
 
-function urlFor(jobName, customer) {
+function buildUrlFor(jobName, customer) {
   var url;
   if (customer) {
     url = jenkinsURL + '/buildByToken/buildWithParameters?job=' + jobName + '&token=' + jenkinsToken + '&customer=' + customer;
@@ -24,7 +25,7 @@ module.exports = function(robot) {
     var customer = msg.match[1];
     if (customer == "demo" || customer == "development") { customer = "demo" };
 
-    robot.http(urlFor(jobName, customer)).post(null) (function(err, response, body) {
+    robot.http(buildUrlFor(jobName, customer)).post(null) (function(err, response, body) {
       if (err) {
         return msg.reply("I can't do that right now");
       } else {
@@ -37,7 +38,7 @@ module.exports = function(robot) {
     var jobName = "Get%20DB%20dump%20for%20developer";
     var customer = msg.match[1];
 
-    robot.http(urlFor(jobName, customer)).post(null) (function(err, response, body) {
+    robot.http(buildUrlFor(jobName, customer)).post(null) (function(err, response, body) {
       if (err) {
         return msg.reply("I can't do that right now");
       } else {
@@ -46,11 +47,16 @@ module.exports = function(robot) {
     });
   })
 
-  robot.respond(/start the ETL for (\w+)/, function(msg) {
+  robot.respond(/start the ETL for (\w+)( using buckets ([^ ]+))?/, function(msg) {
     var customer = msg.match[1];
+    var import_bucket_override = msg.match[3];
     var jobName = "ETL%2fhubot%20etl%20trigger";
+    var url = buildUrlFor(jobName, customer);
+    if (import_bucket_override) {
+      url = url + '&import_bucket_override=' + escape(import_bucket_override);
+    }
 
-    robot.http(urlFor(jobName, customer)).post(null) (function(err, response, body) {
+    robot.http(url).post(null) (function(err, response, body) {
       if (err) {
         return msg.reply("I can't do that right now");
       } else {
@@ -63,7 +69,7 @@ module.exports = function(robot) {
     var customer = msg.match[1];
     var jobName = "ETL%2fhubot%20elasticsearch%20trigger";
 
-    robot.http(urlFor(jobName, customer)).post(null) (function(err, response, body) {
+    robot.http(buildUrlFor(jobName, customer)).post(null) (function(err, response, body) {
       if (err) {
         return msg.reply("I can't do that right now");
       } else {
@@ -75,7 +81,7 @@ module.exports = function(robot) {
   robot.respond(/restart the elasticsearch cluster/i, function(msg) {
     var jobName = "Restart%20elasticsearch%20cluster";
 
-    robot.http(urlFor(jobName)).post(null) (function(err, response, body) {
+    robot.http(buildUrlFor(jobName)).post(null) (function(err, response, body) {
       if (err) {
         return msg.reply("I can't do that right now");
       } else {
